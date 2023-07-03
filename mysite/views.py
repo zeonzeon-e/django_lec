804f21c7afdb1e4bc46a2ff77d7f97b53d7ad480
likelionknu <https://github.com/likelionknu/calendar-react-frontend>
2023-07-03 10:33:19 +0900

    2주차 django 개발 시작

---
+++ mysite/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import MainContent
+
+def index(request):
+    content_list = MainContent.objects.order_by('-pub_date')
+    context = {'content_list': content_list}
+    return render(request, 'mysite/content_list.html', context)
